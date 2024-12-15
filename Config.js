46b773afa0632c42d47e7292782b9a6538512a7c
Nuwan <shanubaby82@gmail.com>
2024-12-15 18:14:18 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+    ALIVE_IMG: process.env.ALIVE_IMG || "   https://i.postimg.cc/BQ8KSkgX/Screenshots-2024-12-12-22-32-15-1.jpg ",
+    ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now here ",
 };
